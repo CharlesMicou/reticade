a072e0dc88fb45b0406ef5b4bfdfb61d8818a4df
Charles Micou <charlesmicou@gmail.com>
2022-04-18 21:44:18 +0100

    WIP: decoder serialisation

--- reticade/decoder_harness.py
+++ reticade/decoder_harness.py
@@ -1,3 +1,18 @@
+import json
+from reticade.decoders import sig_proc
+from reticade.decoders import dummy_decoder
+import logging
+
+# Note(charlie): this is hacky -- a neater solution would be to reflect
+# and case match against Class.__name__.
+known_pipeline_stages = {
+    'Downsampler': sig_proc.Downsampler,
+    'DoGFilter': sig_proc.DoGFilter,
+    'DeltaFFilter': sig_proc.DeltaFFilter,
+    'Flatten': sig_proc.Flatten,
+    'Dummy': dummy_decoder.MeanValueTaker,
+}
+
 class DecoderPipeline:
     def __init__(self, pipeline):
         assert(len(pipeline) > 0)
@@ -8,3 +23,22 @@ class DecoderPipeline:
         for step in self.pipeline_stages:
             next_stage_input = step.process(next_stage_input)
         return next_stage_input
+
+    def make_stage(json_obj):
+        name = json_obj['name']
+        params = json_obj['params']
+        if name not in known_pipeline_stages:
+            logging.error(f"Can't load decoder. Unknown decoder stage: {name}")
+            return
+        return known_pipeline_stages[name].from_json(params)
+
+    def from_json(json_file):
+        with open(json_file, 'r') as file:
+            top_level = json.load(file)
+            pipeline = [DecoderPipeline.make_stage(t) for t in top_level]
+        return DecoderPipeline(pipeline)
+
+    def to_json(self, out_file):
+        with open(out_file, 'w') as f:
+            json.dump([p.to_json() for p in self.pipeline_stages], f)
+        logging.warn(f"Wrote decoder to: {out_file}")
---
+++ reticade/decoders/movement_controller.py
@@ -0,0 +1,12 @@
+class ClassMovementController:
+    def __init__(self, max_acceleration, min_velocity, max_velocity):
+        self.max_acceleration = max_acceleration
+        self.frame_rate = 0 # todo
+
+    def make_from_data(training_data, num_classes):
+        positions, _ = training_data
+        # make velocity conversion here
+        return ClassMovementController(0, 0, 0)
+
+    def process(self, raw_input):
+        return 1.0
