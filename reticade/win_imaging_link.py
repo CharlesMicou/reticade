1dced0b8f9f3e40f6046bddf7c7df1a9019d5d77
Charles Micou <charlesmicou@gmail.com>
2022-05-05 22:57:25 +0100

    WIP: figuring out marshalling

--- reticade/win_imaging_link.py
+++ reticade/win_imaging_link.py
@@ -11,6 +11,8 @@ This imaging link is too slow to render at 30 Hz because of Prairie View's
 long response times. However, it's a useful debugging tool to validate
 that the raw data stream parsed by SharedMemImagingLink is working.
 """
+
+
 class ImagingLink:
     def __init__(self, channel_number):
         self.channel_number = channel_number
@@ -36,20 +38,25 @@ class SharedMemImagingLink:
         prairie_pixels_per_line = self.prairie_link.PixelsPerLine()
         prairie_lines_per_frame = self.prairie_link.LinesPerFrame()
         if prairie_pixels_per_line != image_size[0]:
-            logging.warn(f"Expected {image_size[0]} pixels per line but PrarieView reported {prairie_pixels_per_line}")
+            logging.warn(
+                f"Expected {image_size[0]} pixels per line but PrarieView reported {prairie_pixels_per_line}")
         if prairie_lines_per_frame != image_size[1]:
-            logging.warn(f"Expected {image_size[1]} pixels per line but PrarieView reported {prairie_lines_per_frame}")
+            logging.warn(
+                f"Expected {image_size[1]} pixels per line but PrarieView reported {prairie_lines_per_frame}")
 
         self.pview_samples_per_pixel = self.prairie_link.SamplesPerPixel()
         # Configure shared memory space
-        frame_count = 1
         bytes_per_pixel = 2  # 16 bit integers
         memsize_bytes = image_size[0] * \
-            image_size[1] * frame_count * bytes_per_pixel
+            image_size[1] * bytes_per_pixel * \
+            self.pview_samples_per_pixel
         self.memory_block = shared_memory.SharedMemory(
             create=True, size=memsize_bytes)
+
+        # Multiple line scans
+        data_layout = (image_size[0], self.pview_samples_per_pixel, image_size[1])
         self.shared_array = np.ndarray(
-            (image_size[0], image_size[1]), dtype=np.int16, buffer=self.memory_block.buf)
+            data_layout, dtype=np.int16, buffer=self.memory_block.buf)
         # Force-fill the array on creation so it's not populated by garbage in memory
         self.shared_array.fill(0)
 
@@ -59,17 +66,23 @@ class SharedMemImagingLink:
             logging.error("Failed to enable streaming via PrairieView script")
 
         self.pid = os.getpid()
-        self.num_samples = image_size[0] * image_size[1]
+        self.num_samples_per_frame = image_size[0] * \
+            image_size[1] * self.pview_samples_per_pixel
         Buffer = ctypes.c_char * memsize_bytes
         buf = Buffer.from_buffer(self.memory_block.buf)
         self.sharedmem_addr = ctypes.addressof(buf)
 
     def get_current_frame(self):
         self._send_prairieview_rrd()
-        return self.shared_array.astype(np.float64)
+        # Axis corresponds to multiple sample axis
+        # todo(charlie): is this axis actually correct?
+        # todo(charlie): why is the minimum value reported actually 7000ish?
+        # I can't come up with a sane reason for this
+        return np.mean(self.shared_array, axis=1)
 
     def _send_prairieview_rrd(self):
-        num_samples_written = self.prairie_link.ReadRawDataStream_3(self.pid, self.sharedmem_addr, self.num_samples)
+        num_samples_written = self.prairie_link.ReadRawDataStream_3(
+            self.pid, self.sharedmem_addr, self.num_samples_per_frame)
         if num_samples_written > 0:
             # todo(charlie): verbose logging mode should track this.
             pass
