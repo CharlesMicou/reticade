30daa68b4ccd74ba304ee9dae1fbd1d3deb2d74f
Charles Micou <charlesmicou@gmail.com>
2023-04-19 21:36:29 +0100

    Start writing threading logic

--- reticade/run_ui.py
+++ reticade/run_ui.py
@@ -1,6 +1,11 @@
 import ui_server
 import time
 from multiprocessing import Process, Manager, Pipe
+from interprocess_messages import ProcessMessage
+import logging
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s',
+                    datefmt='%H:%M:%S', level=logging.INFO)
 
 def run_flask(harness_pipe, imaging_pipe, shared_dict):
     app = ui_server.create_flask_app(harness_pipe, imaging_pipe, shared_dict)
@@ -11,14 +16,52 @@ def run_harness(flask_pipe, shared_dict):
         shared_dict['harness_busy'] = False
         instruction = flask_pipe.recv()
         shared_dict['harness_busy'] = True
-        
-        time.sleep(5)
-        shared_dict['labview_connected'] = True
-        flask_pipe.send("Done")
+        instruction_id = instruction[0]
+        if instruction_id == ProcessMessage.SEND_CONNECT_LABVIEW:
+            labview_ip = instruction[1]
+            shared_dict['labview_connected'] = True
+            # TODO: implement me
+            flask_pipe.send((ProcessMessage.ACK_CONNECT_LABVIEW))
+        elif instruction_id == ProcessMessage.SEND_TEST_LABVIEW:
+            # TODO: implement me
+            flask_pipe.send((ProcessMessage.ACK_TEST_LABVIEW))
+            pass
+        elif instruction_id == ProcessMessage.SEND_RUN_BMI:
+            duration = instruction[1]
+            # TODO: implement me
+            pass
+        elif instruction_id == ProcessMessage.SEND_LOAD_DECODER:
+            decoder_name = instruction[1]
+            shared_dict['decoder'] = decoder_name
+            # TODO: implement me
+            flask_pipe.send((ProcessMessage.ACK_LOAD_DECODER))
+        elif instruction_id == ProcessMessage.SEND_TEST_PRAIRIEVIEW:
+            # TODO: implement me
+            time.sleep(5)
+            pass
+        else:
+            logging.error(f"Harness received unexpected instruction: {instruction}")
+            pass
+
 
 
 def run_imaging(flask_pipe, shared_dict):
-    pass
+    while True:
+        shared_dict['imaging_busy'] = False
+        instruction = flask_pipe.recv()
+        shared_dict['imaging_busy'] = True
+        instruction_id = instruction[0]
+        if instruction_id == ProcessMessage.SEND_CONNECT_PRAIRIEVIEW:
+            shared_dict['prairieview_connected'] = True
+            # TODO: implement me
+            logging.info("Conneting")
+            flask_pipe.send((ProcessMessage.ACK_CONNECT_PRAIRIEVIEW))
+        elif instruction_id == ProcessMessage.SEND_TEST_PRAIRIEVIEW:
+            # TODO: implement me
+            time.sleep(5)
+            pass
+        else:
+            logging.error(f"Imaging process received unexpected instruction: {instruction}")
 
 if __name__ == '__main__':
     with Manager() as manager:
@@ -36,7 +79,10 @@ if __name__ == '__main__':
 
         flask_process = Process(target=run_flask, args=(flask_to_harness, flask_to_imaging, shared_dict))
         harness_process = Process(target=run_harness, args=(harness_from_flask, shared_dict))
-        flask_process.start()
+        imaging_process = Process(target=run_imaging, args=(imaging_from_flask, shared_dict))
         harness_process.start()
+        imaging_process.start()
+        flask_process.start()
         harness_process.join()
+        imaging_process.join()
         flask_process.join()
--- reticade/ui_server.py
+++ reticade/ui_server.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect, url_for, request
 from pybars import Compiler
 from multiprocessing import Pipe
+from interprocess_messages import ProcessMessage
+import datetime
 import time
+import logging
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s',
+                    datefmt='%H:%M:%S', level=logging.INFO)
 
 def create_flask_app(harness_pipe, imaging_pipe, shared_dict):
     with open('reticade/static/index.html', 'r') as f:
@@ -15,6 +21,7 @@ def create_flask_app(harness_pipe, imaging_pipe, shared_dict):
 
     @app.route('/')
     def index():
+        shared_dict['refresh_time'] = datetime.datetime.now().strftime('%H:%M:%S')
         html = template(templated_variables)
         return html
 
@@ -22,44 +29,91 @@ def create_flask_app(harness_pipe, imaging_pipe, shared_dict):
     @app.route('/init_labview', methods=["POST"])
     def init_labview():
         labview_ip_addr = request.form['ip_addr']
-        print(f"connecting to {labview_ip_addr}")
-        instruction = "Foo"
-        harness_pipe.send(instruction)
-        done = harness_pipe.recv()
+        logging.info(f"UI Request: initialise labview, data: {labview_ip_addr}")
+        if shared_dict['labview_connected'] == True:
+            logging.warn("Labview is already connected, ignoring request")
+        elif shared_dict['harness_busy'] == True:
+            logging.warn("Harness is busy, ignoring request")
+        else:
+            instruction = (ProcessMessage.SEND_CONNECT_LABVIEW, labview_ip_addr)
+            harness_pipe.send(instruction)
+            done = harness_pipe.recv()
+            if done != ProcessMessage.ACK_CONNECT_LABVIEW:
+                logging.error(f"Expected ACK_CONNECT_LABVIEW, received {done}")
         return redirect(url_for('index'))
 
 
     @app.route('/init_prairieview')
     def init_prairieview():
-        print("init pview")
-        templated_variables['prairieview_connected'] = True
+        logging.info("UI Request: initialise prairieview")
+        if shared_dict['prairieview_connected'] == True:
+            logging.warn("Imaging already configured, ignoring request")
+        elif shared_dict['imaging_busy'] == True:
+            logging.warn("Imaging process is busy,ignoring request")
+        else:
+            imaging_pipe.send((ProcessMessage.SEND_CONNECT_PRAIRIEVIEW, 0))
+            done = imaging_pipe.recv()
+            if done != ProcessMessage.ACK_CONNECT_PRAIRIEVIEW:
+                logging.error(f"Expected ACK_CONNECT_PRAIRIEVIEW, received: {done}")
         return redirect(url_for('index'))
 
 
     @app.route('/load_decoder', methods=["POST"])
     def load_decoder():
-        print("loading decoder")
-        decoder_name = request.form['decoderfile']
-        templated_variables['decoder'] = decoder_name
+        logging.info("UI Request: load decoder")
+        if shared_dict['harness_busy'] == True:
+            logging.warn("Harness is busy, ignoring request")
+        else:
+            decoder_name = request.form['decoderfile']
+            instruction = (ProcessMessage.SEND_LOAD_DECODER, decoder_name)
+            harness_pipe.send(instruction)
+            done = harness_pipe.recv()
+            if done != ProcessMessage.ACK_LOAD_DECODER:
+                logging.error(f"Expected ACK_LOAD_DECODER, received: {done}")
         return redirect(url_for('index'))
 
 
     @app.route('/test_labview')
     def test_labview():
-        print("test labview")
+        logging.info("UI Request: test labview")
+        if shared_dict['harness_busy'] == True:
+            logging.warn("Harness is busy, ignoring request")
+        else:
+            instruction = (ProcessMessage.SEND_TEST_LABVIEW, 5)
+            harness_pipe.send(instruction)
+            done = harness_pipe.recv()
+            if done != ProcessMessage.ACK_TEST_LABVIEW:
+                logging.error(f"Expected ACK_TEST_LABVIEW, received: {done}")
+    
         return redirect(url_for('index'))
 
 
     @app.route('/test_prairieview')
     def test_prairieview():
-        print("test pview")
+        duration_s = 10
+        logging.info("UI Request: test imaging")
+        if shared_dict['harness_busy'] == True or shared_dict['imaging_busy'] == True:
+            logging.warn("ReTiCaDe is busy, ignoring request")
+        else:
+            instruction_1 = (ProcessMessage.SEND_TEST_PRAIRIEVIEW, duration_s)
+            instruction_2 = (ProcessMessage.SEND_TEST_PRAIRIEVIEW, duration_s)
+            harness_pipe.send(instruction_1)
+            imaging_pipe.send(instruction_2)
         return redirect(url_for('index'))
 
 
     @app.route('/run_bmi', methods=["POST"])
     def run_bmi():
-        duration = request.form['duration']
-        print(f"Request to run BMI for {duration} s")
+        duration_s = float(request.form['duration'])
+        print(f"UI Request: Run BMI for {duration_s} s")
+        if shared_dict['harness_busy'] == True or shared_dict['imaging_busy'] == True:
+            logging.warn("ReTiCaDe is busy, ignoring request")
+        else:
+            instruction_1 = (ProcessMessage.SEND_RUN_BMI, duration_s)
+            instruction_2 = (ProcessMessage.SEND_RUN_BMI, duration_s)
+            imaging_pipe.send(instruction_1)
+            time.sleep(2) # Let microscope warm up
+            harness_pipe.send(instruction_2)
         return redirect(url_for('index'))
     
     return app
